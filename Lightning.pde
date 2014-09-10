dbdb041ed6e2f712ae7bf004ead5fcb5225cf5c7
MostExcellent <dwglover3@gmail.com>
2014-09-10 11:14:17 -0700

    Makes lightning

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,43 @@
+
+Bolt bolt;
 void setup()
 {
   size(300,300);
+  bolt = new Bolt((int)(Math.random()*40+130),0);
 }
 void draw()
 {
-
+	bolt.make();
+	if(mousePressed){
+		background(255);
+		bolt = new Bolt((int)(Math.random()*40+130), 0);
+		bolt.make();
+	}
 }
-void mousePressed()
+class Bolt
 {
+	int x;
+	int y;
+	int newX;
+	int newY;
+
+	Bolt (int startX, int startY)
+	{
+		x = startX;
+		y = startY;
+	}
+	void make()
+	{
+		stroke(0);
+		fill(0);
+		while(y <= 300)
+		{
+			newX = x + (int)(Math.random()*20-10);
+			newY = y + (int)(Math.random()*13+3);
+			line(x, y, newX, newY);
+			x = newX;
+			y = newY;
+		}
+	}
 
 }
-
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,21 +14,51 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+
+Bolt bolt;
 public void setup()
 {
   size(300,300);
-  noLoop();
+  bolt = new Bolt((int)(Math.random()*40+130),0);
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+	bolt.make();
+	if(mousePressed){
+		background(255);
+		bolt = new Bolt((int)(Math.random()*40+130), 0);
+		bolt.make();
+	}
 }
-public void mousePressed()
+class Bolt
 {
-  redraw();
-}
+	int x;
+	int y;
+	int newX;
+	int newY;
 
+	Bolt (int startX, int startY)
+	{
+		x = startX;
+		y = startY;
+	}
+	public void make()
+	{
+		stroke(0);
+		fill(0);
+		while(y <= 300)
+		{
+			newX = x + (int)(Math.random()*20-10);
+			newY = y + (int)(Math.random()*13+3);
+			line(x, y, newX, newY);
+			x = newX;
+			y = newY;
+			println("newX: "+newX);
+			println("newY: "+newY);
+		}
+	}
+
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Lightning" };
     if (passedArgs != null) {
